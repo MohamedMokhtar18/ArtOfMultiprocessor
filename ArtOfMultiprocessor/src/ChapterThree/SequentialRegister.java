ccbcd8f7af52a55f8854696aea385023650b4c22
MohamedMokhtar18 <welleamy@gmail.com>
2022-01-10 02:39:50 +0200

    -add SequentialRegister class

---
+++ ArtOfMultiprocessor/src/ChapterThree/SequentialRegister.java
@@ -0,0 +1,11 @@
+package ChapterThree;
+
+public class SequentialRegister<T> implements Register<T> {
+private T value;
+public T read() {
+return value;
+}
+public void write(T v) {
+value = v;
+}
+}
