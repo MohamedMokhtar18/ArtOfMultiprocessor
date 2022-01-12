4b8ed908e6f992f10a6d934c42a9b165e7e1bbf3
MohamedMokhtar18 <welleamy@gmail.com>
2022-01-12 12:54:39 +0200

    -add node class in chapter 5

---
+++ ArtOfMultiprocessor/src/ChapterFive/Node.java
@@ -0,0 +1,35 @@
+package ChapterFive;
+
+import java.lang.Comparable;
+
+import ChapterFour.Consensus;
+import ChapterFour.QueueConsensus;
+
+
+public class Node implements Comparable{
+	public Invoc invoc; // method name and args
+	public Consensus<Node> decideNext; // decide next Node in list
+	public Node next; // the next node
+	public int seq; // sequence number
+
+	public Node(Invoc invoc) {
+		invoc = invoc;
+		decideNext=new QueueConsensus();
+		//decideNext = new ConsensusProtocol<Node>(); it wouldn't compile
+		seq = 0;
+	}
+
+	public static Node max(Node[] array) {
+		Node max = array[0];
+		for (int i = 1; i < array.length; i++)
+			if (max.seq < array[i].seq)
+				max = array[i];
+		return max;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
