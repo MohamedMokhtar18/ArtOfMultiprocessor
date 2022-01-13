de41c136d77d959d871dda6325042e29f478dc70
MohamedMokhtar18 <welleamy@gmail.com>
2022-01-13 03:12:21 +0200

    -add test and lock class

---
+++ ArtOfMultiprocessor/src/ChapterSix/TASLock.java
@@ -0,0 +1,43 @@
+package ChapterSix;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class TASLock implements Lock {
+	AtomicBoolean state = new AtomicBoolean(false);
+
+	public void lock() {
+		while (state.getAndSet(true)) {
+		}
+	}
+
+	public void unlock() {
+		state.set(false);
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean tryLock() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Condition newCondition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
