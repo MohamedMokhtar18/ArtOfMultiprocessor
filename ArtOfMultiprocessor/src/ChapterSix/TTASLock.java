7560f80aacfe5e1f4638ab0d583483c175ce5b51
MohamedMokhtar18 <welleamy@gmail.com>
2022-01-13 03:14:15 +0200

    -add Test,Test and set

---
+++ ArtOfMultiprocessor/src/ChapterSix/TTASLock.java
@@ -0,0 +1,48 @@
+package ChapterSix;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class TTASLock implements Lock {
+	AtomicBoolean state = new AtomicBoolean(false);
+
+	public void lock() {
+		while (true) {
+			while (state.get()) {
+			}
+			;
+			if (!state.getAndSet(true))
+				return;
+		}
+	}
+
+	public void unlock() {
+		state.set(false);
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean tryLock() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Condition newCondition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
